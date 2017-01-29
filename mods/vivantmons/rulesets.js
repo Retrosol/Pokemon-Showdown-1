36f08eebd995745f5a14c7efa4c40c547c287428
Retrosol <r3trosol@gmail.com>
2017-01-29 17:30:04 -0600

    Delete rulesets.js

--- mods/vivantmons/rulesets.js
+++
@@ -1,7 +0,0 @@
-exports.BattleFormats = {
-	standard: {
-		effectType: 'Banlist',
-		ruleset: ['Sleep Clause Mod', 'Species Clause', 'Nickname Clause', 'OHKO Clause', 'Moody Clause', 'Evasion Moves Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Cancel Mod'],
-		banlist: ['Unreleased', 'Illegal']
-	}
-};
